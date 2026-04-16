a84c729f72511d261367d48634104fe55b5e9778
KumarRishi <rishir00788@gmail.com>
2026-04-17 02:21:51 +0530

    Add datatypes example in C++
    This file demonstrates the use of various data types in C++, including integers, doubles, characters, booleans, and strings.

---
+++ Basic code/datatypes.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main(){
+    system("cls");
+
+    // integer (whole number)
+   int age = 25;
+   int year = 2026;
+   int day = 7;
+
+   // double (number including decimal)
+   double cgpa = 8.10;
+   double price = 2.87;
+   double temp = 40.32;
+
+    // character (single character)
+    char grade = 'A';
+    char symbol = '#';
+    char letter = 'D';
+
+    // boolean (true or false)
+    bool student = true;
+    bool teacher = false;
+    bool raining = true;
+
+    // string 
+    string name = "rishi";
+    string city = "bhopal";
+    string country = "india";
+
+    cout << "hello " << name << '\n';
+    cout << "im in "<< city << '\n';
+    cout << "i live in " << country << '\n';
+
+
+    return 0;
+}
