fd9fddfd90527efab701dc4e4564de58ab38efc7
KumarRishi <rishir00788@gmail.com>
2026-04-18 19:08:19 +0530

    Add basic C++ program with intro function

---
+++ Basic code/functions.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void intro(int number){
+string name = "rishi";
+int age = 21;
+char grade = 'A';
+cout<<name<<endl;
+cout<<age<<endl;
+cout<< grade<<endl;
+cout<< number;
+}
+
+int main(){
+int number = 694486986;
+intro(number);
+
+return 0;
+}
