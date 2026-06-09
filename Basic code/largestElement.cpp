c6afe5529a2d0ebc83fa3fda1b8debf16a22845c
KumarRishi <rishir00788@gmail.com>
2026-06-09 17:30:51 +0530

    Add C++ program to find largest element in array

---
+++ Basic code/largestElement.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[] = {12, 45, 2, 91, 23};
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    int max = arr[0]; 
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i]; 
+        }
+    }
+    cout << "Largest element is: " << max << endl;
+    return 0;
+}
