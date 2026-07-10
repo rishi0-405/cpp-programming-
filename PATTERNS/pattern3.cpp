479b74497ee3f94cf20b866c51855be7c895d17e
KumarRishi <rishir00788@gmail.com>
2026-07-11 01:52:07 +0530

    Add pattern printing program in pattern3.cpp
    This program prints a pattern of consecutive uppercase letters based on user input.

---
+++ PATTERNS/pattern3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    char start = 'A';
+    while(i<=n){
+        int j=1;
+        while (j<=n){
+            cout<<start;
+            j=j+1;
+         start=start+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+}
